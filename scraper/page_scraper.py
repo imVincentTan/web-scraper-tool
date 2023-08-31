b41d2f57616402b6d02ab001fbb4ba334bec9752
Vincent <vincent.tan.1998@gmail.com>
2023-08-30 23:32:46 -0700

    basic scrape using selenium (url hidden)

--- scraper/page_scraper.py
+++ scraper/page_scraper.py
@@ -1,2 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import json
+
 if __name__ == '__main__':
-    print('hi world')
+    browser = webdriver.Chrome()
+    url = ''
+    browser.get(url)
+
+    elements = browser.find_elements(By.CLASS_NAME, "job-link")
+
+    jobs = []
+
+    for e in elements:
+        job_details = {}
+        job_details['link'] = e.get_attribute('href')
+        job_details['posting-date'] = e.find_element(By.CLASS_NAME, 'posting-date').text
+        job_details['last-updated'] = e.find_element(By.CLASS_NAME, 'time-elapsed').text
+        jobs.append(job_details)
+
+    print(json.dumps(jobs, indent=4))
