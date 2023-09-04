32b3bcaaeb3d0cb029dd8732846689a0cda41722
Vincent <vincent.tan.1998@gmail.com>
2023-09-04 15:50:07 -0700

    extract website specific search terms to some json add some simple parsing to parse the json

--- scraper/page_scraper.py
+++ scraper/page_scraper.py
@@ -1,21 +1,59 @@
+from typing import Any
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import json
 
+data = [
+    {
+        'id': '',
+        'url': '',
+        'listing_element': [['find_elements', 'class', 'job-link']],
+        'targets': {
+            'link': [['get_attribute', 'href']],
+            'posting_date': [['find_element', 'class', 'posting-date'], ['text']],
+            'last_updated': [['find_element', 'class', 'time-elapsed'], ['text']]
+        }
+    }
+]
+
+def get_by_from_attribute(attribute):
+    retval: Any
+    match attribute:
+        case 'class':
+            retval = By.CLASS_NAME
+    return retval
+    
+def eval_command(element, command):
+    retval: Any
+    match command[0]:
+        case 'find_elements':
+            retval = element.find_elements(get_by_from_attribute(command[1]), command[2])
+        case 'find_element':
+            retval = element.find_element(get_by_from_attribute(command[1]), command[2])
+        case 'get_attribute':
+            retval = element.get_attribute(command[1])
+        case 'text':
+            retval = element.text
+    return retval
+
+def parse_selection(element, commands):
+    retval = element
+    for command in commands:
+        retval = eval_command(retval, command)
+    return retval
+
 if __name__ == '__main__':
     browser = webdriver.Chrome()
-    url = ''
-    browser.get(url)
-
-    elements = browser.find_elements(By.CLASS_NAME, "job-link")
-
     jobs = []
 
-    for e in elements:
-        job_details = {}
-        job_details['link'] = e.get_attribute('href')
-        job_details['posting-date'] = e.find_element(By.CLASS_NAME, 'posting-date').text
-        job_details['last-updated'] = e.find_element(By.CLASS_NAME, 'time-elapsed').text
-        jobs.append(job_details)
+    for d in data:    
+        browser.get(d['url'])
+        elements = parse_selection(browser, d['listing_element'])
+        for e in elements:
+            job_details = {'id': d['id']}
+            job_details['targets'] = {}
+            for t in d['targets']:
+                job_details['targets'][t] = parse_selection(e, d['targets'][t])
+            jobs.append(job_details)
 
     print(json.dumps(jobs, indent=4))
